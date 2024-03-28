f681b25078af41ec43f840f6ddbffc010e15c60b
unknown <1008051@mymail.sutd.edu.sg>
2024-03-28 09:17:49 +0800

    Hanging Animation

--- Assets/Scripts/MonkeyBar.cs
+++ Assets/Scripts/MonkeyBar.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MonkeyBar : MonoBehaviour
 {
+	[SerializeField] private Color newestMat = new Color(0,2,255);
+	[SerializeField] private Color newerMat = new Color(0,1,255);
 	[SerializeField] private Color newMat = new Color(0,0,255);
 	[SerializeField] private Color oldMat = new Color(0,0,0);
 	[SerializeField] private List<Material> bar;
 	[SerializeField] private Animator anim;
+	[SerializeField] private GameObject hang;
 
     // Start is called before the first frame update
     void Start()
@@ -45,5 +48,24 @@ public class MonkeyBar : MonoBehaviour
 		{
 			bar[i].color = oldMat;
 		}
+	}
+
+	void LightChange()
+	{
+		for (int i = 0; i < bar.Count; i++)
+		{
+			newerMat = new Color(Mathf.Lerp(255,0,1),1,255);
+			bar[i].color = newerMat;
+		}
+	}
+
+	void EnableHang()
+	{
+		hang.SetActive(true);
+	}
+
+	void DisableHang()
+	{
+		hang.SetActive(false);
 	}
 }
